318ede0abbc5247771f67cb762e9cda907d10557
Akshell <aksh3ll@gmail.com>
2025-12-03 07:35:34 +0100

    2025: start

---
+++ java/src/main/java/fr/akshell/aoc/y2025/Day1.java
@@ -0,0 +1,19 @@
+package fr.akshell.aoc.y2025;
+
+import fr.akshell.aoc.base.BaseDay;
+
+import java.util.List;
+
+public class Day1 extends BaseDay<Integer> {
+
+    @Override
+    public Integer part1(String input) {
+        List<String> lines = input.lines().toList();
+        return 0;
+    }
+
+    @Override
+    public Integer part2(String input) {
+        return 0;
+    }
+}
